5f8a2f8e9d12248adc1280bd5544ca8a989fe31b
ekmekjian <ekmekjian.ara@protonmail.com>
2021-05-02 13:08:49 -0400

    Add files via upload
    scripts for new installs on i3 manjaro

---
+++ freshmoviein.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo pacman -S tmux gradle zathura libreoffice-still pulseaudio pavucontrol pa-applet yakuake discord lib32-gnutls lib32-libldap lib32-libgpg-error lib32-sqlite lib32-libpulse
