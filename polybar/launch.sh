717990175195f5c55d63342eb759cc33f6491c7b
ekmekjian <ekmekjian.ara@protonmail.com>
2021-08-15 15:16:19 -0400

    Update launch.sh

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,5 +6,5 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar example &
-killall xembedsniproxy
+polybar main &
+polybar secondary &
